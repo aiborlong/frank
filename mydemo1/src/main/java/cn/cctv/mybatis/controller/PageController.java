21e8a417f92f1ae77626cfa1e0053334a4d63d31
aiborlong <aiborlong@yeah.net>
2017-07-08 13:18:29 +0800

    first demo 2017-07-08

---
+++ mydemo1/src/main/java/cn/cctv/mybatis/controller/PageController.java
@@ -0,0 +1,11 @@
+package cn.cctv.mybatis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("Page")
+@Controller
+public class PageController {
+    
+
+}
